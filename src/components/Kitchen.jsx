5f453b2b14009deb3e63083b12d270b9d9d44c7f
aradelraycia <129363766+aradelraycia@users.noreply.github.com>
2023-04-26 01:00:09 +0800

    Completed the kitchen part

--- src/components/Kitchen.jsx
+++ src/components/Kitchen.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
-import Title from "./Title"
 import Spinner from "./Spinner"
+import TitleOverlay from './TitleOverlay';
 
 function Kitchen({data}) {
   if(!data || data.length === 0) {
@@ -13,29 +13,28 @@ function Kitchen({data}) {
 
   return (
     <>
-      <div className="container d-flex flex-column align-items-end">
-        <Title subTitle={'kitchen and dining'} mainTitle={'Minimalist Blueprint'} />
+    <div className='title-container'>
+      <TitleOverlay mainTitle={'kitchen & dining'} subTitle={'minimalist blueprint'} bgImg={data.kitchenImgs[0]} />
+    </div>
 
-        <div className="row d-flex align-items-center" id="kitchen-description">
-          <div className="col-md-4">
-            <h1 className="text-secondary">Aesthetically Designed</h1>
-            <p className="text-secondary">Built with the best furniture and appliances to compliment the minimalist, modern look</p>
+    <div className="kitchen-grid">
+      <div className="container">
+        <div className="row d-flex">
+
+        <div className="col-md-6 d-flex flex-column text-white">
+          <div className="kitchen-description">
+            <h3>Aesthetically Designed</h3>
+            <p>From the cozy dining area and spacious kitchen to the carefully chosen finishes and fixtures, every element of this space has been carefully curated to create a warm and inviting ambiance</p>
           </div>
-          <div className="col-md-8">
-          <a href={data.kitchenImgs[0]} data-toggle="lightbox" data-gallery="img-gallery">
-            <img src={data.kitchenImgs[0]} alt="kitchen" className="w-100" />
-          </a>
-          </div>
+          <div className="kitchen-photo1" style={{background: `url(${data.kitchenImgs[1]})`}}></div>
         </div>
+        <div className="col-md-6 kitchen-photo2" style={{background: `url(${data.kitchenImgs[0]})`}}>
 
-        <div className="row mt-5 align-self-start w-100" id="dining">
-          <div className="col-md-8 ">
-          <a href={data.kitchenImgs[1]} data-toggle="lightbox" data-gallery="img-gallery">
-            <img src={data.kitchenImgs[1]} alt="dining" className="w-100" />
-          </a>
-          </div>    
+        </div>
         </div>
       </div>
+    </div>
+      
     </>
   )
 }
