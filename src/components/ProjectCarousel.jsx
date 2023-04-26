34060d8cadf5c61887aa10eb4ea36094678a2254
aradelraycia <129363766+aradelraycia@users.noreply.github.com>
2023-04-27 01:26:19 +0800

    Fixed the animation settings on ProjectCarousel.jsx

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import Spinner from './Spinner'
 import Carousel from 'react-bootstrap/Carousel';
 
-function ProjectCarousel({variant, data, imgs}) {
+function ProjectCarousel({ data, imgs}) {
 
   if(!data || data.length === 0) {
     return <Spinner />
@@ -17,7 +17,7 @@ function ProjectCarousel({variant, data, imgs}) {
     <Carousel slide={false} indicators={false} interval={5000} nextLabel={''} prevLabel={''}>
       {imgs.map((item, index)=> (
         <Carousel.Item key={index}>
-          <a href={item} data-toggle="lightbox" data-gallery="img-gallery" data-aos="fade-left" data-aos-delay="550" data-aos-duration="500">
+          <a href={item} data-toggle="lightbox" data-gallery="img-gallery" data-aos="fade-left" data-aos-delay="500" data-aos-duration="500">
             <img
               className="d-block w-100"
               src={item}
@@ -32,13 +32,11 @@ function ProjectCarousel({variant, data, imgs}) {
 }
 
 ProjectCarousel.defaultProps = {
-  variant: 'no-caption',
   data: {},
   imgs: []
 }
 
 ProjectCarousel.propTypes = {
-  variant: PropTypes.string,
   data: PropTypes.object.isRequired,
   imgs: PropTypes.array.isRequired
 }
