3b1baea94748eb1029debc85d1beaf4aa761030a
aradelraycia <129363766+aradelraycia@users.noreply.github.com>
2023-05-02 00:05:02 +0800

    Added Not Found Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import HousingProject from "./pages/HousingProject";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/project/:projectName/:id' element={<HousingProject />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
     </Router>
 
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+function NotFoundPage() {
+  return (
+    <div id="not-found" className="bg-gray d-flex align-items-center">
+      <div className="container d-flex align-items-center flex-column">
+        <h1 className="text-primary">Page not found</h1>
+        <p className="text-lighterGray mt-2">The page you are trying to visit does not seem to exist</p>
+        
+        <p className="text-lighterGray">Go back <Link to={'/'} className="text-primary">home</Link></p>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
